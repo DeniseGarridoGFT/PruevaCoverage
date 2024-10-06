054c2c910748956ecfbec37f4fc0510c29b1e0f9
denise <degm@gft.com>
2024-10-06 13:22:49 +0200

    scafoldign 10.2.6

--- src/test/java/PersonaTest.java
+++ src/test/java/PersonaTest.java
@@ -6,7 +6,7 @@ public class PersonaTest {
 
     @Test
     public void testPersona() {
-        Persona persona = new Persona("Norly", 1500);
+        Persona persona = new Persona("Norly", 1400);
         assertEquals("Norly", persona.getNombre());
     }
 }
